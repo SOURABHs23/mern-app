834f3291e58ec79e111a564bd16d23f003ea1cca
SOURABHs23 <sourabhgarg873@gmail.com>
2024-09-17 16:15:23 +0530

    testing server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
 import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("app is running");
+});
+
+app.listen(5000, () => {
+  console.log("server is running on 5000");
+});
