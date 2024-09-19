ca2a31432e8fbdc15204f167c75a16cb2a20c12b
SOURABHs23 <sourabhgarg873@gmail.com>
2024-09-19 20:23:04 +0530

    backend done, now starting with frontend

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -31,8 +31,9 @@ router.get("/", async (req, res) => {
 //GET SINGLE USER
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
+  console.log(id);
   try {
-    const singleUser = await userData.findById({ _id: id });
+    const singleUser = await UserModel.findById({ _id: id });
     res.status(200).json(singleUser);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -43,11 +44,23 @@ router.get("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const deletedUser = await userData.findByIdAndDelete({ _id: id });
+    const deletedUser = await UserModel.findByIdAndDelete({ _id: id });
     res.status(201).json(deletedUser);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
 
+router.patch("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, email, about } = req.body;
+  try {
+    const updateUser = await UserModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(201).json(updateUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 export default router;
